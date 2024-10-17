885528a08a40f6d64782cd760d0a035c4b7ec250
syjeon0608 <syjeon0608@gmail.com>
2024-10-17 18:36:17 +0900

    refactor: 테스트코드 리팩토링

--- src/test/java/com/hhpl/concertreserve/domain/payment/PointTest.java
+++ src/test/java/com/hhpl/concertreserve/domain/payment/PointTest.java
@@ -1,7 +1,6 @@
 package com.hhpl.concertreserve.domain.payment;
 
 import com.hhpl.concertreserve.domain.error.BusinessException;
-import com.hhpl.concertreserve.domain.error.BusinessExceptionCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,28 +8,28 @@ import static com.hhpl.concertreserve.domain.error.BusinessExceptionCode.INVALID
 import static com.hhpl.concertreserve.domain.error.BusinessExceptionCode.POINT_NOT_ENOUGH;
 import static org.junit.jupiter.api.Assertions.*;
 
-class WalletTest {
+class PointTest {
 
     @Test
     @DisplayName("충전한 금액만큼 잔액이 증가해야 한다.")
     void testChargeIncreasesAmount() {
-        Wallet wallet = new Wallet(1L, 10000);
+        Point point = new Point(1L, 10000);
         int chargeToAmount = 50000;
 
-        wallet.charge(chargeToAmount);
+        point.chargePoints(chargeToAmount);
 
-        assertEquals(60000, wallet.getAmount());
-        assertNotNull(wallet.getUpdatedAt());
+        assertEquals(60000, point.getAmount());
+        assertNotNull(point.getUpdatedAt());
     }
 
     @Test
     @DisplayName("충전 금액은 0원 이상이여야한다.")
     void shouldThrowExceptionWhenChargeAmountIsZero() {
-        Wallet wallet = new Wallet(1L, 10000);
+        Point point = new Point(1L, 10000);
         int amountToCharge = 0;
 
         BusinessException exception = assertThrows(BusinessException.class, () -> {
-            wallet.charge(amountToCharge);
+            point.chargePoints(amountToCharge);
         });
 
         assertEquals(INVALID_CHARGE_AMOUNT, exception.getErrorCode());
@@ -39,11 +38,11 @@ class WalletTest {
     @Test
     @DisplayName("충전 금액은 음수면 안된다.")
     void shouldThrowExceptionWhenChargeAmountIsNegative() {
-        Wallet wallet = new Wallet(1L, 10000);
+        Point point = new Point(1L, 10000);
         int amountToCharge = -1000;
 
         BusinessException exception = assertThrows(BusinessException.class, () -> {
-            wallet.charge(amountToCharge);
+            point.chargePoints(amountToCharge);
         });
 
         assertEquals(INVALID_CHARGE_AMOUNT, exception.getErrorCode());
@@ -53,11 +52,11 @@ class WalletTest {
     @Test
     @DisplayName("잔액이 부족하면 예외를 발생시킨다.")
     void shouldThrowExceptionWhenBalanceIsInsufficient() {
-        Wallet wallet = new Wallet(1L, 50000);
+        Point point = new Point(1L, 50000);
         int amountToUse = 60000;
 
         BusinessException exception = assertThrows(BusinessException.class, () -> {
-            wallet.checkCanUse(amountToUse);
+            point.checkPointsForPayment(amountToUse);
         });
 
         assertEquals(POINT_NOT_ENOUGH, exception.getErrorCode());
@@ -66,12 +65,12 @@ class WalletTest {
     @Test
     @DisplayName("결제 후 잔액에서 결제 금액만큼 차감한다.")
     void shouldDeductAmountFromBalanceAfterPayment() {
-        Wallet wallet = new Wallet(1L, 70000);
+        Point point = new Point(1L, 70000);
         int amountToUse = 20000;
 
-        wallet.useToPayment(amountToUse);
+        point.usePointToPayment(amountToUse);
 
-        assertEquals(50000,wallet.getAmount());
+        assertEquals(50000, point.getAmount());
     }
 
 }
