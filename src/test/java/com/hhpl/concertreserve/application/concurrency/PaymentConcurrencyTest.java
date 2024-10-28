10d7255821919616e7e19e38ae4cb016fd25c59f
syjeon0608 <syjeon0608@gmail.com>
2024-10-28 19:57:53 +0900

    fix: 결제 동시성 제어 문제 해결

--- src/test/java/com/hhpl/concertreserve/application/concurrency/PaymentConcurrencyTest.java
+++ src/test/java/com/hhpl/concertreserve/application/concurrency/PaymentConcurrencyTest.java
@@ -1,5 +1,6 @@
 package com.hhpl.concertreserve.application.concurrency;
 
+import com.hhpl.concertreserve.application.facade.PaymentFacade;
 import com.hhpl.concertreserve.domain.concert.model.Concert;
 import com.hhpl.concertreserve.domain.concert.model.Reservation;
 import com.hhpl.concertreserve.domain.concert.model.Schedule;
@@ -7,11 +8,15 @@ import com.hhpl.concertreserve.domain.concert.model.Seat;
 import com.hhpl.concertreserve.domain.concert.type.SeatStatus;
 import com.hhpl.concertreserve.domain.error.CoreException;
 import com.hhpl.concertreserve.domain.payment.PaymentService;
+import com.hhpl.concertreserve.domain.user.UserRepository;
+import com.hhpl.concertreserve.domain.user.model.Point;
+import com.hhpl.concertreserve.domain.waitingqueue.model.WaitingQueue;
 import com.hhpl.concertreserve.infra.database.concert.ConcertJpaRepository;
 import com.hhpl.concertreserve.infra.database.concert.ReservationJpaRepository;
 import com.hhpl.concertreserve.infra.database.concert.ScheduleJpaRepository;
 import com.hhpl.concertreserve.infra.database.concert.SeatJpaRepository;
 import com.hhpl.concertreserve.infra.database.payment.PaymentJpaRepository;
+import com.hhpl.concertreserve.infra.database.waitingqueue.WaitingQueueJpaRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,14 +50,29 @@ public class PaymentConcurrencyTest {
     @Autowired
     PaymentService paymentService;
 
+    @Autowired
+    PaymentFacade paymentFacade;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private WaitingQueueJpaRepository waitingQueueJpaRepository;
+
+
     @Test
     @DisplayName("사용자가 5번 결제 요청하면 1번만 성공한다.")
     void testConcurrentPaymentSuccessOnReservation() throws InterruptedException {
-        Concert  concert = concertJpaRepository.save(new Concert(1L, "Test Concert", "Description", LocalDateTime.now(), LocalDateTime.now().plusHours(3), LocalDateTime.now().plusDays(1)));
-        Schedule  schedule = scheduleJpaRepository.save(new Schedule(1L, concert, LocalDateTime.now().plusDays(1), 100, 100));
-        Seat seat = seatJpaRepository.save(new Seat(1L, schedule, 100, SeatStatus.AVAILABLE, LocalDateTime.now().plusDays(1),0L));
+        WaitingQueue waitingQueue = WaitingQueue.createWithQueueNo("uuid777", 5L, 777L);
+        waitingQueue.activate();
+        waitingQueueJpaRepository.save(waitingQueue);
+        Point point = new Point(9L,20000);
+        userRepository.updatePoint(point);
+        Concert  concert = concertJpaRepository.save(new Concert(5L, "Test Concert", "Description", LocalDateTime.now(), LocalDateTime.now().plusHours(3), LocalDateTime.now().plusDays(1)));
+        Schedule  schedule = scheduleJpaRepository.save(new Schedule(2L, concert, LocalDateTime.now().plusDays(1), 100, 100));
+        Seat seat = seatJpaRepository.save(new Seat(2L, schedule, 200, SeatStatus.AVAILABLE, LocalDateTime.now().plusDays(1),10L));
         seatJpaRepository.save(seat);
-        Reservation reservation = new Reservation("uuid", seat);
+        Reservation reservation = new Reservation("uuid777", seat);
         reservationJpaRepository.save(reservation);
 
         int numberOfThreads = 5;
@@ -65,7 +85,7 @@ public class PaymentConcurrencyTest {
         for (int i = 0; i < numberOfThreads; i++) {
             executorService.submit(() -> {
                 try {
-                    paymentService.completePayment(1L,reservation,100 );
+                    paymentFacade.processPayment(1L,9L,"uuid777" );
                     successfulRegistrations[0]++;
                 } catch (CoreException e) {
                     failedRegistrations[0]++;
