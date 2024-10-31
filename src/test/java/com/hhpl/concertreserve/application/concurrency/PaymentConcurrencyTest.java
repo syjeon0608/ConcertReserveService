49a6672da81ae8bcb701bb7acb116731fb355a49
syjeon0608 <syjeon0608@gmail.com>
2024-10-31 21:13:09 +0900

    test: 포인트 충전 후 즉시 결제 테스트 케이스 추가

--- src/test/java/com/hhpl/concertreserve/application/concurrency/PaymentConcurrencyTest.java
+++ src/test/java/com/hhpl/concertreserve/application/concurrency/PaymentConcurrencyTest.java
@@ -9,7 +9,9 @@ import com.hhpl.concertreserve.domain.concert.type.SeatStatus;
 import com.hhpl.concertreserve.domain.error.CoreException;
 import com.hhpl.concertreserve.domain.payment.PaymentService;
 import com.hhpl.concertreserve.domain.user.UserRepository;
+import com.hhpl.concertreserve.domain.user.UserService;
 import com.hhpl.concertreserve.domain.user.model.Point;
+import com.hhpl.concertreserve.domain.user.model.PointStatus;
 import com.hhpl.concertreserve.domain.waitingqueue.model.WaitingQueue;
 import com.hhpl.concertreserve.infra.database.concert.ConcertJpaRepository;
 import com.hhpl.concertreserve.infra.database.concert.ReservationJpaRepository;
@@ -59,6 +61,10 @@ public class PaymentConcurrencyTest {
     @Autowired
     private WaitingQueueJpaRepository waitingQueueJpaRepository;
 
+    @Autowired
+    private UserService userService;
+
+
 
     @Test
     @DisplayName("사용자가 5번 결제 요청하면 1번만 성공한다.")
@@ -66,11 +72,11 @@ public class PaymentConcurrencyTest {
         WaitingQueue waitingQueue = WaitingQueue.createWithQueueNo("uuid777", 5L, 777L);
         waitingQueue.activate();
         waitingQueueJpaRepository.save(waitingQueue);
-        Point point = new Point(9L,20000);
+        Point point = new Point(9L,30000);
         userRepository.updatePoint(point);
         Concert  concert = concertJpaRepository.save(new Concert(5L, "Test Concert", "Description", LocalDateTime.now(), LocalDateTime.now().plusHours(3), LocalDateTime.now().plusDays(1)));
         Schedule  schedule = scheduleJpaRepository.save(new Schedule(2L, concert, LocalDateTime.now().plusDays(1), 100, 100));
-        Seat seat = seatJpaRepository.save(new Seat(2L, schedule, 200, SeatStatus.AVAILABLE, LocalDateTime.now().plusDays(1),10L));
+        Seat seat = seatJpaRepository.save(new Seat(2L, schedule, 50000, SeatStatus.AVAILABLE, LocalDateTime.now().plusDays(1),10L));
         seatJpaRepository.save(seat);
         Reservation reservation = new Reservation("uuid777", seat);
         reservationJpaRepository.save(reservation);
@@ -85,7 +91,7 @@ public class PaymentConcurrencyTest {
         for (int i = 0; i < numberOfThreads; i++) {
             executorService.submit(() -> {
                 try {
-                    paymentFacade.processPayment(reservation.getId(),9L,"uuid777" );
+                    paymentFacade.processPayment(reservation.getId(),point.getUserId(),"uuid777" );
                     successfulRegistrations[0]++;
                 } catch (CoreException e) {
                     failedRegistrations[0]++;
@@ -101,6 +107,53 @@ public class PaymentConcurrencyTest {
 
         assertEquals(1, successfulRegistrations[0], "한번만 결제에 성공해야한다");
         assertEquals(4, failedRegistrations[0], "나머지 결제는 다 실패");
+
+    }
+
+
+    @Test
+    @DisplayName("사용자가 충전 후 결제를 하면 금액이 모두 정확하게 반영된다")
+    void testConcurrentChargeAndUse() throws InterruptedException {
+        WaitingQueue waitingQueue = WaitingQueue.createWithQueueNo("uuid777", 5L, 777L);
+        waitingQueue.activate();
+        waitingQueueJpaRepository.save(waitingQueue);
+        Point point = new Point(10L,50000);
+        userRepository.updatePoint(point);
+        Concert  concert = concertJpaRepository.save(new Concert(5L, "Test Concert", "Description", LocalDateTime.now(), LocalDateTime.now().plusHours(3), LocalDateTime.now().plusDays(1)));
+        Schedule  schedule = scheduleJpaRepository.save(new Schedule(2L, concert, LocalDateTime.now().plusDays(1), 100, 100));
+        Seat seat = seatJpaRepository.save(new Seat(2L, schedule, 80000, SeatStatus.AVAILABLE, LocalDateTime.now().plusDays(1),10L));
+        seatJpaRepository.save(seat);
+        Reservation reservation = new Reservation("uuid777", seat);
+        reservationJpaRepository.save(reservation);
+
+        int numberOfThreads = 1;
+        int[] successfulRegistrations = {0};
+        int[] failedRegistrations = {0};
+
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            executorService.submit(() -> {
+                try {
+                    userService.updateUserPoint(point.getUserId(), 100000, PointStatus.CHARGE);
+                    paymentFacade.processPayment(reservation.getId(),point.getUserId(),"uuid777" );
+                    successfulRegistrations[0]++;
+                } catch (CoreException e) {
+                    failedRegistrations[0]++;
+                    System.err.println("Error for user " + Thread.currentThread().getName() + ": " + e.getMessage());
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        executorService.shutdown();
+
+        Point updatedPoint = userService.getUserPoint(point.getUserId());
+
+        assertEquals(70000,updatedPoint.getAmount());
 
     }
 }
