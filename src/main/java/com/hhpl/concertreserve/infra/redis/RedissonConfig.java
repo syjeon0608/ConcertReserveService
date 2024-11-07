454cd1d8761d94e9e478347ad8696feff439f8ef
syjeon0608 <syjeon0608@gmail.com>
2024-11-07 10:32:35 +0900

    add: redissonConfig 추가

--- src/main/java/com/hhpl/concertreserve/infra/redis/RedissonConfig.java
+++ src/main/java/com/hhpl/concertreserve/infra/redis/RedissonConfig.java
@@ -2,6 +2,7 @@ package com.hhpl.concertreserve.infra.redis;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
 import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ public class RedissonConfig {
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
+        config.setCodec(new StringCodec());
         config.useSingleServer()
                 .setAddress("redis://localhost:6379");
         return Redisson.create(config);
