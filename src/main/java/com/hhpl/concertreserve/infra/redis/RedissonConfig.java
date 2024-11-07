4afee113523237c8f8bc0b1dde07a07bd0171f61
syjeon0608 <syjeon0608@gmail.com>
2024-11-08 00:12:18 +0900

    fix: 레디스 직렬화 문제 해결

--- src/main/java/com/hhpl/concertreserve/infra/redis/RedissonConfig.java
+++ src/main/java/com/hhpl/concertreserve/infra/redis/RedissonConfig.java
@@ -1,8 +1,14 @@
 package com.hhpl.concertreserve.infra.redis;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
-import org.redisson.client.codec.StringCodec;
+import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +19,17 @@ public class RedissonConfig {
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.setCodec(new StringCodec());
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType(Object.class)
+                .build();
+        mapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL);
+
+        config.setCodec(new JsonJacksonCodec(mapper));
         config.useSingleServer()
                 .setAddress("redis://localhost:6379");
         return Redisson.create(config);
