c5012c8525a88f6e63a4e5f75ab74ab7926893af
syjeon0608 <syjeon0608@gmail.com>
2024-11-17 19:48:37 +0900

    refactor: 분리된 설정 클래스로 @EnableAsync, @EnableScheduling 이동

---
+++ src/main/java/com/hhpl/concertreserve/config/common/AsyncConfig.java
@@ -0,0 +1,10 @@
+package com.hhpl.concertreserve.config.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+}
---
+++ src/main/java/com/hhpl/concertreserve/config/common/SchedulingConfig.java
@@ -0,0 +1,10 @@
+package com.hhpl.concertreserve.config.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+
+}
