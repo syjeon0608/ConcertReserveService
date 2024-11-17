586afccd79b3e11b7ba42dff7ee19afac0b612d9
syjeon0608 <syjeon0608@gmail.com>
2024-11-17 21:49:31 +0900

    refactor: redis Config수정. @class 메타데이터 제거, ObjectMapper주입

--- src/main/java/com/hhpl/concertreserve/config/database/RedisConfig.java
+++ src/main/java/com/hhpl/concertreserve/config/database/RedisConfig.java
@@ -1,37 +1,46 @@
 package com.hhpl.concertreserve.config.database;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.hhpl.concertreserve.app.concert.domain.entity.Concert;
+import lombok.RequiredArgsConstructor;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
-import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.codec.TypedJsonJacksonCodec;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+    @Value("${spring.data.redis.protocol:redis}")
+    private String protocol;
+
+    private final ObjectMapper objectMapper;
 
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
 
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
-                .allowIfSubType(Object.class)
-                .build();
-        mapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL);
+        TypeReference<List<Concert>> typeReference = new TypeReference<>() {};
+        TypedJsonJacksonCodec codec = new TypedJsonJacksonCodec(typeReference, objectMapper);
 
-        config.setCodec(new JsonJacksonCodec(mapper));
-        config.useSingleServer()
-                .setAddress("redis://localhost:6379");
+        config.setCodec(codec);
+        config.useSingleServer().setAddress(protocol + "://" + host + ":" + port);
+
         return Redisson.create(config);
     }
+
 }
